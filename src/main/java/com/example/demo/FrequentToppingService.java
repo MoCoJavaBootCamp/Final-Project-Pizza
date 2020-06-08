c44104788f94baffa356b8e131534fdb1c345072
eyasuh <eyha12@gmail.com>
2020-06-08 14:09:26 -0400

    first branch commit

---
+++ src/main/java/com/example/demo/FrequentToppingService.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FrequentToppingService {
+
+	private final FrequentToppingRepo frequentToppingRepo;
+
+	public FrequentToppingService(FrequentToppingRepo frequentToppingRepo) {
+		this.frequentToppingRepo = frequentToppingRepo;
+	}
+
+	public Pizza findMostCommonTopping() {
+		List<FrequentToppings> frequentToppings = frequentToppingRepo.findToppingCount();
+		return frequentToppings.get(0).getCar();
+	}
+
+}
