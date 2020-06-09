3b5c8b32bb3202eac69b7248a71d90a5cc7c9a80
azdoolittle <drewdoolittle@gmail.com>
2020-06-09 16:57:41 -0400

    repo methods

--- src/main/java/com/example/demo/PizzaRepository.java
+++ src/main/java/com/example/demo/PizzaRepository.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface PizzaRepository extends CrudRepository<Pizza, Long> {
+    Set<Pizza> findAll();
 }
