46b3ce38e39043ed923a7afdb0cea99329da728d
adoolittle <drewdoolittle@gmail.com>
2020-06-22 23:15:51 -0400

    react refactor setup

---
+++ src/main/js/app.js
@@ -0,0 +1,22 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {employees: []};
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: '/api/employees'}).done(response => {
+			this.setState({employees: response.entity._embedded.employees});
+		});
+	}
+
+	render() {
+		return (
+			<EmployeeList employees={this.state.employees}/>
+		)
+	}
+}
